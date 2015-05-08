6bdc40183ca1f80937bc5373dbf87a719f78ffda
fxsjy <ccnusjy@gmail.com>
2015-05-08 12:52:51 +0800

    use unix style end of line

--- storage/meta.cc
+++ storage/meta.cc
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string>
 #include <boost/filesystem.hpp>
+#include "common/logging.h"
 
 namespace galaxy {
 namespace ins {
@@ -13,50 +14,56 @@ const std::string term_file_name = "term.data";
 const std::string vote_file_name = "vote.data";
 
 Meta::Meta(const std::string& data_dir) : data_dir_(data_dir),
-										  term_file_(NULL),
-										  vote_file_(NULL) {
-	if (!bf::exists(data_dir)) {
-		bf::create_directories(data_dir);
-	}
-	term_file_ = fopen((data_dir+"/"+term_file_name).c_str(), "a+");
-	vote_file_ = fopen((data_dir+"/"+vote_file_name).c_str(), "a+");
-	assert(term_file_);
-	assert(vote_file_);
+                                          term_file_(NULL),
+                                          vote_file_(NULL) {
+    if (!bf::exists(data_dir)) {
+        bf::create_directories(data_dir);
+    }
+    term_file_ = fopen((data_dir+"/"+term_file_name).c_str(), "a+");
+    vote_file_ = fopen((data_dir+"/"+vote_file_name).c_str(), "a+");
+    assert(term_file_);
+    assert(vote_file_);
 }
 
 Meta::~Meta() {
-	fclose(term_file_);
-	fclose(vote_file_);
+    fclose(term_file_);
+    fclose(vote_file_);
 }
 
 int64_t Meta::ReadCurrentTerm() {
-	int64_t cur_term = 0, tmp = 0;
-	while(fscanf(term_file_, "%ld", &tmp) == 1) {
-		cur_term = tmp;
-	}
-	return cur_term;
+    int64_t cur_term = 0, tmp = 0;
+    while(fscanf(term_file_, "%ld", &tmp) == 1) {
+        cur_term = tmp;
+    }
+    return cur_term;
 }
 
 void Meta::ReadVotedFor(std::map<int64_t, std::string>& voted_for) {
-	voted_for.clear();
-	int64_t term = 0;
-	char server_id[1024] = {'\0'};
-	while(fscanf(vote_file_, "%ld %s", &term, server_id) == 2) {
-		voted_for[term] = std::string(server_id);
-	}
+    voted_for.clear();
+    int64_t term = 0;
+    char server_id[1024] = {'\0'};
+    while(fscanf(vote_file_, "%ld %1023s", &term, server_id) == 2) {
+        voted_for[term] = std::string(server_id);
+    }
 }
 
 void Meta::WriteCurrentTerm(int64_t current_term) {
-	fprintf(term_file_, "%ld\n", current_term);
-	fflush(term_file_);	
+    fprintf(term_file_, "%ld\n", current_term);
+    if (fflush(term_file_) != 0) {
+        LOG(FATAL, "Meta::WriteCurrentTerm failed, term:%ld", current_term);
+        abort();
+    }
 }
 
 void Meta::WriteVotedFor(int64_t term, const std::string& server_id) {
-	fprintf(vote_file_, "%ld %s\n", term, server_id.c_str());
-	fflush(vote_file_);
+    fprintf(vote_file_, "%ld %s\n", term, server_id.c_str());
+    if (fflush(vote_file_) != 0) {
+        LOG(FATAL, "Meta::WriteVotedFor failed, term:%ld, server_id:%s",
+            term, server_id.c_str());
+        abort();
+    }
 }
 
 } //namespace ins
 } //namespace galaxy
-
 
--- storage/meta.h
+++ storage/meta.h
@@ -8,16 +8,16 @@ namespace galaxy {
 namespace ins {
 class Meta {
 public:
-	Meta(const std::string& data_dir);
-	~Meta();
-	int64_t ReadCurrentTerm();
-	void ReadVotedFor(std::map<int64_t, std::string>& voted_for);
-	void WriteCurrentTerm(int64_t term); 
-	void WriteVotedFor(int64_t term, const std::string& server_id);
+    Meta(const std::string& data_dir);
+    ~Meta();
+    int64_t ReadCurrentTerm();
+    void ReadVotedFor(std::map<int64_t, std::string>& voted_for);
+    void WriteCurrentTerm(int64_t term); 
+    void WriteVotedFor(int64_t term, const std::string& server_id);
 private:
-	std::string data_dir_;
-	FILE* term_file_;
-	FILE* vote_file_;
+    std::string data_dir_;
+    FILE* term_file_;
+    FILE* vote_file_;
 };
 
 } //namespace ins
