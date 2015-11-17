d9a54eeb159a18d811b13504b75d43af27295b47
Kai-Zhang <cs.zhangkai@outlook.com>
2015-11-17 19:37:08 +0800

    Fix user manage test and update COMAKE

--- server/user_manage_test.cc
+++ server/user_manage_test.cc
@@ -11,8 +11,8 @@ TEST(UserManageTest, CommonUserTest) {
     root.set_passwd("rootpassword");
     UserManager user_manager("/tmp/user_test1", root);
     // Try to login an inexisting user
-    std::string uuid1, uuid2, uuid3, rootid;
-    Status ret = user_manager.Login("user1", "123456", &uuid1);
+    std::string uuid1 = UserManager::CalcUuid("user1");
+    Status ret = user_manager.Login("user1", "123456", uuid1);
     EXPECT_EQ(ret, kUnknownUser);
     // Try to logout an inexisting user
     ret = user_manager.Logout("someuuid");
@@ -34,32 +34,30 @@ TEST(UserManageTest, CommonUserTest) {
     ret = user_manager.Register("user1", "abcdefg");
     EXPECT_EQ(ret, kUserExists);
     // Login
-    ret = user_manager.Login("user1", "123456", &uuid1);
+    ret = user_manager.Login("user1", "123456", uuid1);
     EXPECT_EQ(ret, kOk);
-    EXPECT_TRUE(!uuid1.empty());
     EXPECT_TRUE(user_manager.IsLoggedIn(uuid1));
     // Login again
-    ret = user_manager.Login("user1", "123456", &uuid2);
+    std::string uuid2 = UserManager::CalcUuid("user1");
+    ret = user_manager.Login("user1", "123456", uuid2);
     EXPECT_EQ(ret, kOk);
     // And logout
     ret = user_manager.Logout(uuid2);
     EXPECT_EQ(ret, kOk);
-    uuid2 = "";
     // Wrong password
-    ret = user_manager.Login("user2", "abcdefg", &uuid2);
+    uuid2 = UserManager::CalcUuid("user2");
+    ret = user_manager.Login("user2", "abcdefg", uuid2);
     EXPECT_EQ(ret, kPasswordError);
-    EXPECT_TRUE(uuid2.empty());
-    ret = user_manager.Login("user2", "123456", &uuid2);
+    ret = user_manager.Login("user2", "123456", uuid2);
     EXPECT_EQ(ret, kOk);
-    EXPECT_TRUE(!uuid2.empty());
     EXPECT_TRUE(user_manager.IsLoggedIn(uuid2));
-    ret = user_manager.Login("user3", "123456", &uuid3);
+    std::string uuid3 = UserManager::CalcUuid("user3");
+    ret = user_manager.Login("user3", "123456", uuid3);
     EXPECT_EQ(ret, kOk);
-    EXPECT_TRUE(!uuid3.empty());
     EXPECT_TRUE(user_manager.IsLoggedIn(uuid3));
-    ret = user_manager.Login("root", "rootpassword", &rootid);
+    std::string rootid = UserManager::CalcUuid("root");
+    ret = user_manager.Login("root", "rootpassword", rootid);
     EXPECT_EQ(ret, kOk);
-    EXPECT_TRUE(!rootid.empty());
     EXPECT_TRUE(user_manager.IsLoggedIn(rootid));
     // Logout
     ret = user_manager.Logout(uuid1);
@@ -73,9 +71,9 @@ TEST(UserManageTest, CommonUserTest) {
     EXPECT_EQ(ret, kOk);
     ret = user_manager.Logout(rootid);
     EXPECT_EQ(ret, kOk);
-    ret = user_manager.Login("user1", "123456", &uuid1);
+    ret = user_manager.Login("user1", "123456", uuid1);
     EXPECT_EQ(ret, kOk);
-    ret = user_manager.Login("user2", "123456", &uuid2);
+    ret = user_manager.Login("user2", "123456", uuid2);
     EXPECT_EQ(ret, kOk);
     // Delete myself
     ret = user_manager.DeleteUser(uuid1, "user1");
@@ -93,8 +91,8 @@ TEST(UserManageTest, SuperUserTest) {
     root.set_passwd("rootpassword");
     UserManager user_manager("/tmp/user_test2", root);
     // Login root
-    std::string rootid;
-    Status ret = user_manager.Login("root", "rootpassword", &rootid);
+    std::string rootid = UserManager::CalcUuid("root");
+    Status ret = user_manager.Login("root", "rootpassword", rootid);
     EXPECT_EQ(ret, kOk);
     // Create some targets
     ret = user_manager.Register("user1", "123456");
@@ -103,10 +101,11 @@ TEST(UserManageTest, SuperUserTest) {
     EXPECT_EQ(ret, kOk);
     ret = user_manager.Register("user3", "123456");
     EXPECT_EQ(ret, kOk);
-    std::string uuid1, uuid2;
-    ret = user_manager.Login("user1", "123456", &uuid1);
+    std::string uuid1 = UserManager::CalcUuid("user1");
+    ret = user_manager.Login("user1", "123456", uuid1);
     EXPECT_EQ(ret, kOk);
-    ret = user_manager.Login("user2", "123456", &uuid2);
+    std::string uuid2 = UserManager::CalcUuid("user2");
+    ret = user_manager.Login("user2", "123456", uuid2);
     EXPECT_EQ(ret, kOk);
     // Permission denied test
     ret = user_manager.ForceOffline(uuid1, "user2");
@@ -129,7 +128,7 @@ TEST(UserManageTest, SuperUserTest) {
     ret = user_manager.TruncateOnlineUsers(rootid);
     EXPECT_EQ(ret, kOk);
     EXPECT_TRUE(!user_manager.IsLoggedIn(uuid1));
-    ret = user_manager.Login("user1", "123456", &uuid1);
+    ret = user_manager.Login("user1", "123456", uuid1);
     EXPECT_EQ(ret, kOk);
     // Force user offline
     ret = user_manager.ForceOffline(rootid, "user1");
@@ -154,18 +153,15 @@ TEST(UserManageTest, ToolFunctionTest) {
     root.set_username("root");
     root.set_passwd("rootpassword");
     UserManager user_manager("/tmp/user_test3", root);
-    std::string uuid1, uuid2;
     Status ret = user_manager.Register("user1", "123456");
     EXPECT_EQ(ret, kOk);
     ret = user_manager.Register("user2", "123456");
     EXPECT_EQ(ret, kOk);
-    // Test CalcUuid
-    uuid1 = UserManager::CalcUuid("user1");
-    ret = user_manager.Login("user1", "123456", &uuid2);
+    std::string uuid1 = UserManager::CalcUuid("user1");
+    ret = user_manager.Login("user1", "123456", uuid1);
     EXPECT_EQ(ret, kOk);
-    EXPECT_EQ(UserManager::CalcName(uuid1), UserManager::CalcName(uuid2));
     // Test user_manager.IsLoggedIn
-    EXPECT_TRUE(user_manager.IsLoggedIn(uuid2));
+    EXPECT_TRUE(user_manager.IsLoggedIn(uuid1));
     EXPECT_TRUE(!user_manager.IsLoggedIn("user3"));
     // Test user_manager.IsValidUser
     EXPECT_TRUE(user_manager.IsValidUser("user1"));
